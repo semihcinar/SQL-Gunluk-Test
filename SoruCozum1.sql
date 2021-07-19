c1298d688ae922c2606d03b68f26a932e943c539
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-07-19 03:56:29 +0300

    Create SoruCozum1.sql

---
+++ SoruCozum1.sql
@@ -0,0 +1,17 @@
+Soru Çözüm 1
+
+--Veri tabaný ismi BikeStores
+
+--soru: sipariþ alýnmayan bölge sorulmaktadýr.
+--tablo isimleri 
+--1) sales.stores
+--2) production.stocks
+--3) production.stocks
+
+
+select state 
+from sales.stores a, production.stocks b, production.products c
+where not  c.product_name='Trek Remedy 9.8 - 2017'
+AND c.product_id = b.product_id 
+and b.store_id = a.store_id
+group by state
